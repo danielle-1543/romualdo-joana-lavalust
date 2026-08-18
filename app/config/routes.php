2b4b058e43a4f38f718b9c7a6abeb6772244151a
Dana Romualdo <danaromualdo09@gmail.com>
2026-08-18 17:38:24 +0800

    update route

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,8 +3,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $router->get('/', 'StudentController::index');
 
-
-$router->get( '/student', 'StudentController::index');
+$router->get('/student', 'StudentController::index');
 
 $router->get('/student/profile', 'StudentController::profile')
        ->middleware('StudentMiddleware');
